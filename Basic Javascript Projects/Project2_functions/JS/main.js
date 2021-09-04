89434ede9465fedb1e782365e51aff02492f1ed7
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-09-04 13:19:02 -0700

    Create main.js

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+function My_First_Function () {//creating a function
+    var str="This text is blue";//creating a string
+    var result= str.fontcolor("blue");//changing that string to blue
+    document.getElementById('Blue_Text').innerHTML = result;//locating the id in the html file
+} 
+
+function myFunction() {//creating a function
+    
+    var sentence="I am learning";//creating a sentence variable 
+    sentence += " A lot from this course!";//concatenating this with the above string
+    document.getElementById("Concatenate").innerHTML = sentence//locating the id in the html file
+}
