b39dd111d78a7a6f7f0a1567894c0589ccbe4d60
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-09-04 12:10:51 -0700

    Create main.js

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,34 @@
+//alert ("Hello world!");
+
+//window.alert('Hello world!'); 
+var A ="This is a string" // assigning a variable 
+var A = A.fontcolor("purple") //assiging a font color to that same variable
+var X ="Hello world"
+var B ="Concatenated" + "string"
+var B = B.fontcolor("orange")
+var Family = "Johnsons", Dad = "Joey", Mom = "Alejandra", Daughter = "Peggy", Son = "Antonio" //making multiples variables at once
+var Family = Family.fontcolor("red")
+var Dad = Dad.fontcolor("brown")
+var blues = "I have the blues"
+var blues = blues.fontcolor("blue")
+
+
+document.write(A);//writing out var A in text on the webpage
+window.alert(X); //opening an alert window with var X
+document.write("<br>");//this is a line break to seperate var A from the below string
+document.write("Lisa told Bart \"Knock it off bart! or I\'ll tell dad!\" <br> \"Eat my shorts! \"Bart responded"
+);//using \ to ignore certain ""
+
+document.write("<br>");
+
+
+document.write ("\"Be who you are and say what you feel," + "because those who mind don\'t matter and those who matter don\'t mind." + "-Dr. Seuss <br>");
+//using concatenated strings
+document.write("<br>");
+document.write(B);
+document.write("<br>");
+document.write( Dad);
+document.write("<br>");
+document.write(blues);
+document.write("<br>");
+document.write(4+5);//writing a statement with an expression
