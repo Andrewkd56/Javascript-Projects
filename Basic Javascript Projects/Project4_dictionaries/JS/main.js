c217a2f6d806bf983b7ab385142d552fc2923b38
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-09-05 11:54:48 -0700

    Create main.js

---
+++ Basic Javascript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,11 @@
+function my_Dictionary() {//creating a function to create a dictionary
+    var Pokemon = {//var is pokemon for what we are describing
+        Type:"Fire",//its type
+        Gender:"Male",//its gender
+        Color:"Orange",
+        Level:36,
+        Name:"Charizard"
+    };
+    delete Pokemon.Type; //here we used an operator as a word to delete part of our dictionary
+    document.getElementById("Dictionary").innerHTML = Pokemon.Type;
+}
